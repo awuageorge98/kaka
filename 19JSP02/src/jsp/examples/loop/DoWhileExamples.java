c79074dc4cefb5179da9fc127b26d18d4fc4c517
awuageorge98 <awuageorge98@gmail.com>
2024-12-26 23:08:12 +0700

    Update DoWhileExamples.java
    fh2

--- 19JSP02/src/jsp/examples/loop/DoWhileExamples.java
+++ 19JSP02/src/jsp/examples/loop/DoWhileExamples.java
@@ -11,7 +11,7 @@ public class DoWhileExamples {
 	public static void main(String args[]) {
 		int a = 1, sum = 0;
         do {
-            sum += a; // sum = sum + a
+            sum += a; // sum = s11um + a
             a++;
         } while (a <= 10);
         System.out.println("Sum of 1 to 10 is : " + sum);
