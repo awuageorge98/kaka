cab62311339cef1926153d0a10b4a4a5f4773a20
awuageorge98 <awuageorge98@gmail.com>
2024-12-30 09:17:08 +0700

    Update HashSetExample1.java
    1243

--- 19JSP02/src/jsp/examples/collections/HashSetExample1.java
+++ 19JSP02/src/jsp/examples/collections/HashSetExample1.java
@@ -16,7 +16,7 @@ public class HashSetExample1 {
         // Creating hashset
         Set<String> set = new HashSet<String>();
         // Add objects to hashset
-        set.add("J111ava222");
+        set.add("J21431ava222");
         set.add("C++");
         set.add("PHP");
         set.add("Java11");
